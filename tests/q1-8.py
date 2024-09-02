08c72cc3f0280ac27e58db85512df8008dc4affa
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-09-02 18:00:06 +0900

    q1

---
+++ tests/q1-8.py
@@ -0,0 +1,12 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1-8',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': ">>> assert calculate_calories(6.0, 30, 75.0) == 225, 'テストケース1失敗: 225 kcal が期待されました'\n", 'hidden': False, 'locked': False},
+                                   {'code': ">>> assert calculate_calories(8.0, 45, 60.0) == 378, 'テストケース2失敗: 324 kcal が期待されました'\n", 'hidden': False, 'locked': False},
+                                   {'code': ">>> assert calculate_calories(4.0, 60, 55.0) == 198, 'テストケース3失敗: 198 kcal が期待されました'\n", 'hidden': False, 'locked': False},
+                                   {'code': ">>> assert calculate_calories(10.0, 20, 70.0) == 252, 'テストケース4失敗: 252 kcal が期待されました'\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
