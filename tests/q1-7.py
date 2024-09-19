f98b01d8259aaf0fe62956d69a7fddfedd6339ee
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-09-19 12:33:35 +0900

    update

--- tests/q1-7.py
+++ tests/q1-7.py
@@ -28,18 +28,6 @@ test = {   'name': 'q1-7',
                                                ">>> assert result['tax'] == 500, '消費税が正しくありません'\n"
                                                ">>> assert result['total_price'] == 5500, '合計価格が正しくありません'\n",
                                        'hidden': False,
-                                       'locked': False},
-                                   {   'code': ">>> result = calculate_order('Grape', 123.45, 10)\n"
-                                               ">>> assert result['product_name'] == 'Grape', '商品名が正しくありません'\n"
-                                               ">>> assert result['unit_price'] == 123.45, '単価が正しくありません'\n"
-                                               ">>> assert result['quantity'] == 10, '数量が正しくありません'\n"
-                                               ">>> assert result['tax_excluded_price'] == 1234.5, '税抜価格が正しくありません'\n"
-                                               ">>> assert result['tax'] == 123, '消費税が正しくありません'\n"
-                                               ">>> assert result['total_price'] == 1357, '合計価格が正しくありません'\n",
-                                       'hidden': False,
-                                       'locked': False},
-                                   {   'code': ">>> result\n{'product_name': 'Grape',\n 'unit_price': 123.45,\n 'quantity': 10,\n 'tax_excluded_price': 1234.5,\n 'tax': 123,\n 'total_price': 1357}",
-                                       'hidden': False,
                                        'locked': False}],
                       'scored': True,
                       'setup': '',
