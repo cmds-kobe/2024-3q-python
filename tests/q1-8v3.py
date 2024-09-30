271dca39948894fde1375182f61d56469dd16e74
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-09-30 19:07:22 +0900

    q1-8-3

---
+++ tests/q1-8v3.py
@@ -0,0 +1,17 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1-8v3',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> assert mets == 4\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert time == 37\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert weight == 75\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert rmr == 3.6\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert calories == 166.5\n', 'hidden': False, 'locked': False},
+                                   {   'code': ">>> assert hmessage == '=========(計算結果)==========\\n運動強度      :        4 METS\\n運動時間      :       37 分\\n体重\\u3000\\u3000      :       75 "
+                                               "kg\\nＲＭＲ\\u3000      :      3.6 kcal/kg/時\\nｋｃａｌ      :    166.5 kcal\\n============================='\n",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
