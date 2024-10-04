151a33fd5245cd8a6821bde5753836078af4affe
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-10-04 14:42:19 +0900

    update

---
+++ tests/q2-6.py
@@ -0,0 +1,15 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2-6',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': ">>> assert fizz_buzz(15) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', 'Bazz', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', 'Bazz', 'FizzBuzz']\n",
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> assert fizz_buzz(22) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', 'Bazz', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', 'Bazz', 'FizzBuzz', '16', '17', "
+                                               "'Fizz', '19', 'Buzz', 'FizzBazz', '22']\n",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
