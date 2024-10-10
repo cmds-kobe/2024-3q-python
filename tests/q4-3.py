67f08ade3e6a6cf485cf81211d3c804c3e2f34e9
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-10-10 18:50:34 +0900

    update

--- tests/q4-3.py
+++ tests/q4-3.py
@@ -5,8 +5,7 @@ test = {   'name': 'q4-3',
     'suites': [   {   'cases': [   {   'code': '>>> import pickle\n'
                                                '>>> import hashlib\n'
                                                '>>> digest = hashlib.sha256(pickle.dumps(ans43.values)).hexdigest()\n'
-                                               ">>> digest == 'c51a71eed44a0fc2e6bd8a7da5b29e31cdc177f31984ebd902dd1cc1000c9a0f'\n"
-                                               'True',
+                                               ">>> digest == 'c51a71eed44a0fc2e6bd8a7da5b29e31cdc177f31984ebd902dd1cc1000c9a0f'\n",
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
---
+++ tests/q4-4v2.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q4-4v2',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': '>>> assert ans44_m == 1127.3\n', 'hidden': False, 'locked': False}, {'code': '>>> assert ans44_s == 1194.9\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
