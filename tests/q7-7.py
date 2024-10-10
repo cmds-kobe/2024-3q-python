1a7171412b54cfa08035a767ca33c28eb8d2c4b7
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-10-10 20:33:43 +0900

    update

--- tests/q7-7.py
+++ tests/q7-7.py
@@ -4,8 +4,6 @@ test = {   'name': 'q7-7',
     'points': 3,
     'suites': [   {   'cases': [   {   'code': '>>> import pickle\n'
                                                '>>> import hashlib\n'
-                                               '>>> ans77_model_digest = hashlib.sha256(pickle.dumps(ans77_model)).hexdigest()\n'
-                                               ">>> assert ans77_model_digest == '1a48107615d99c3433c397904f286d0e859e1147bc10fdc51f5ccacfc486debd'\n"
                                                '>>> ans77_ytest_digest = hashlib.sha256(pickle.dumps(ans77_ytest)).hexdigest()\n'
                                                ">>> assert ans77_ytest_digest == '999be15539009bf3762ec619388c99cfa7f2c0d96215acecc1227f6bcbbc7a2b'\n"
                                                '>>> ans77_ypred_digest = hashlib.sha256(pickle.dumps(ans77_ypred)).hexdigest()\n'
